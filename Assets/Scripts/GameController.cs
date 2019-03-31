1e7f498a0879a5ddc1b3379d2147932a91fb0405
SuperAZ123 <46609772+SuperAZ123@users.noreply.github.com>
2019-03-31 17:06:24 -0400

    Adding files

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardcount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text ScoreText;
+    public Text RestartText;
+    public Text GameOverText;
+    private bool gameover;
+    private bool restart;
+    private int score;
+
+    void Start()
+    {
+        score = 0;
+        gameover = false;
+        restart = false;
+        RestartText.text = "";
+        GameOverText.text = "";
+        UpdateScore();
+        StartCoroutine (SpawnWaves()); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (true)
+        {
+            for (int i = 0; i < hazardcount; i++)
+
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameover)
+            {
+                RestartText.text = "Press 'R' for restart";
+                restart = true;
+                break;
+            }
+        }
+
+    }
+
+    public void addScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        GameOverText.text = "Game Over!";
+        gameover = true;
+    }
+}
